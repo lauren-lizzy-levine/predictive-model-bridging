00c34cdc60e6c8fd5d144e7b41aa79da3e474c78
lauren-lizzy-levine <lauren.lizzy.levine@gmail.com>
2024-03-16 12:24:58 -0400

    code to make data pairs

--- extract_features.py
+++ extract_features.py
@@ -33,7 +33,7 @@ def parse_entity_instance(token_list, index, ent_id):
     entity_text = entity_text[:-1]
 
     # get head token
-    head_token_index = -1
+    head_idx = 0
     token_indexes = []
     token_heads = []
     for token in token_list:
@@ -41,15 +41,15 @@ def parse_entity_instance(token_list, index, ent_id):
         token_heads.append(token["head"])
     for i in range(len(token_list)):
         if token_heads[i] not in token_indexes and token_heads[i] is not None:
-            head_token_index = i
+            head_idx = i
             break
 
-    head_form, head_lemma, head_deprel, head_xpos = token_list[i]["form"], token_list[i]["lemma"],\
-        token_list[i]["deprel"], token_list[i]["xpos"]
+    head_form, head_lemma, head_deprel, head_xpos = token_list[head_idx]["form"], token_list[head_idx]["lemma"],\
+        token_list[head_idx]["deprel"], token_list[head_idx]["xpos"]
 
     head_number = None
-    if token_list[i]["feats"] and "Number" in token_list[i]["feats"]:
-        head_number = token_list[i]["feats"]["Number"]
+    if token_list[head_idx]["feats"] and "Number" in token_list[head_idx]["feats"]:
+        head_number = token_list[head_idx]["feats"]["Number"]
 
     entity_info = {"doc_index": index, "entity_text": entity_text, "entity_id": entity_id, "entity_type": entity_type,
                    "infostat": infostat, "coref_type": coref_type, "coref": coref, "bridge": bridge,
@@ -112,9 +112,10 @@ def extract_entity_instances(conllu_sentences):
     return df
 
 
-def make_instance_file():
+def make_instance_files():
     gum_file_list = glob.glob("dep/*.conllu")
-    df = pd.DataFrame()
+    inst_df = pd.DataFrame()
+    pair_df = pd.DataFrame()
     for file in gum_file_list:
         file_name = file.split("/")[-1].split(".")[0]
         print(file_name)
@@ -126,19 +127,81 @@ def make_instance_file():
         # add file_name and genre
         entity_instances["doc_id"] = file_name
         entity_instances["genre"] = genre
+
+        entity_pairs = make_data_pairs(entity_instances)
         # print(entity_instances.head()
-        # break
-        df = pd.concat([df, entity_instances], ignore_index=True)
-    df.to_csv('gum_entity_instances.csv', sep='\t')
+        inst_df = pd.concat([inst_df, entity_instances], ignore_index=True)
+        pair_df = pd.concat([pair_df, entity_pairs], ignore_index=True)
+        #break
+    inst_df.to_csv('gum_entity_instances.csv', sep='\t')
+    pair_df.to_csv('gum_entity_pairs.csv', sep='\t')
     return
+
+
+def join_rows(first_row, second_row, closest=False):
+
+    bridge = 0
+    coref = 0
+    if second_row["bridge"] and second_row["bridge_from"] == first_row["entity_id"] and closest:
+        bridge = 1
+    if first_row["coref"] and second_row["coref"] and first_row["entity_id"] == second_row["entity_id"]:
+        coref = 1
+
+    pair_info = {"fst_entity_text": first_row["entity_text"], "fst_entity_type": first_row["entity_type"],
+            "fst_infostat": first_row["infostat"], "fst_coref_type": first_row["coref_type"],
+            "fst_head_form": first_row["head_form"], "fst_head_lemma": first_row["head_lemma"],
+            "fst_head_deprel": first_row["head_deprel"], "fst_head_xpos": first_row["head_xpos"],
+            "fst_head_number": first_row["head_number"],
+            "snd_entity_text": second_row["entity_text"], "snd_entity_type": second_row["entity_type"],
+            "snd_infostat": second_row["infostat"], "snd_coref_type": second_row["coref_type"],
+            "snd_head_form": second_row["head_form"], "snd_head_lemma": second_row["head_lemma"],
+            "snd_head_deprel": second_row["head_deprel"], "snd_head_xpos": second_row["head_xpos"],
+            "snd_head_number": second_row["head_number"],
+            "coref": coref, "bridge": bridge}
+
+    return pair_info
 
 
 def make_data_pairs(entity_inst_df):
-    return
+    seen_pairs = set() # doc indexes of instances already paired up
+    pairs = []
+    bridging_instances = entity_inst_df[entity_inst_df["bridge"] == 1]
+    for _, bridge_row in bridging_instances.iterrows():
+        bridge_from_ent_id = bridge_row["bridge_from"]
+        bridge_index = bridge_row["doc_index"]
+        #print(bridge_from_ent_id, bridge_index)
+        bridge_from_ent_id_instances = entity_inst_df[entity_inst_df["entity_id"] == bridge_from_ent_id]
+        # I want the rel_ent closest to the bridge_index but not after it to make a pair
+        # all other intances of this ent with the bridge inst should be marked as not eligble
+        source_ent_indexes = bridge_from_ent_id_instances["doc_index"]
+        closest = None
+        for num in source_ent_indexes:
+            if num < bridge_index:
+                closest = num
+            else:
+                break
+        #print(closest)
+        for _, ent_inst_row in bridge_from_ent_id_instances.iterrows():
+            doc_idx = ent_inst_row["doc_index"]
+            if doc_idx == closest:
+                pair = join_rows(ent_inst_row, bridge_row, closest=True)
+                pairs.append(pair)
+                seen_pairs.add((doc_idx, bridge_index))
+    # get coref and non-coref pairs
+    for _, inst_1 in entity_inst_df.iterrows():
+        for _, inst_2 in entity_inst_df.iterrows():
+            if inst_1["doc_index"] > inst_2["doc_index"]:
+                if (inst_2["doc_index"], inst_1["doc_index"]) not in seen_pairs:
+                    pair = join_rows(inst_2, inst_1)
+                    pairs.append(pair)
+                    seen_pairs.add((inst_2["doc_index"], inst_1["doc_index"]))
+
+    df = pd.DataFrame(pairs)
+    return df
 
 
 def main():
-    make_instance_file()
+    make_instance_files()
     return
 
 
